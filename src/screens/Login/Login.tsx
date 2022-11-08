5e42a63a54246878ec63b7fc1efec816f2e0dd09
Flávio Coutinho <coutinhoflavio20@gmail.com>
2022-11-08 22:42:55 +0100

    feat: configuracao de fonts e temas

---
+++ src/screens/Login/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Login: React.FC = () => {
+    return (
+        <Container>
+            <Text>Scren Login</Text>
+        </Container>
+    )
+}
+
+export { Login };
